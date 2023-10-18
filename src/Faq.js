f61d7ae404e8d376e2925a5298c9b2140520c9e8
Pratibhapa <pratibhaa4321@gmail.com>
2023-10-18 14:42:34 +0545

    faq-template

---
+++ src/Faq.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Faq = () => {
+
+    const faqData = [
+        {
+            question: 'What is React?',
+            answer: 'React is a javascript library for buildig user interfaces',
+        },
+        {
+            question: 'How do I install React?',
+            answer: 'You can install react by using npm or yarn and create a new React application',
+        },
+    ];
+    return (
+        <div className="faq">
+            <h1>Frequently Asked Questions</h1>
+            <ul>
+                {faqData.map((item, index) => (
+                    <li key={index}>
+                            <button className="question">{item.question}</button>
+                            <div className="answer">{item.answer}</div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+      
+
+           
+
+export default Faq;
